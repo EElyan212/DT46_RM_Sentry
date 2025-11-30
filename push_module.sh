8ff0eb3d3890ed476b154840b520aa8d99b3354d
EElyan <shuwenjie91@gmail.com>
2025-12-01 01:07:17 +0800

    1

--- push_module.sh
+++ push_module.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -e  # 出现任何错误立即退出
 
-git subtree pull --prefix=src/rm_driver/livox_ros_drier2 livox_ros_driver2 master
+git subtree pull --prefix=src/rm_driver/livox_ros_driver2 livox_ros_drier2 master
