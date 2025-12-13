3d6d836856b387c55d3979d9b4e76424a344c954
Kielas <c1470759@outlook.com>
2025-12-13 21:16:16 +0800

    创建仿真模型（一）

--- src/rm_perception/linefit_ground_segmentation_ros2/linefit_ground_segmentation_ros/launch/segmentation.launch.py
+++ src/rm_perception/linefit_ground_segmentation_ros2/linefit_ground_segmentation_ros/launch/segmentation.launch.py
@@ -1,30 +1,27 @@
-
 import os
 
 from ament_index_python.packages import get_package_share_directory
-
 from launch import LaunchDescription
 from launch_ros.actions import Node
 
 
-
 def generate_launch_description():
     # Getting directories and launch-files
-    bringup_dir = get_package_share_directory('linefit_ground_segmentation_ros')
-    params_file = os.path.join(bringup_dir, 'launch', 'segmentation_params.yaml')
+    bringup_dir = get_package_share_directory("linefit_ground_segmentation_ros")
+    params_file = os.path.join(bringup_dir, "launch", "segmentation_params.yaml")
 
-    # Nodes launching commands
+    # 注意：这里不要加逗号！
+    env = os.environ.copy()
+    env["LD_PRELOAD"] = "/lib/x86_64-linux-gnu/libusb-1.0.so.0"
+
     node_start_cmd = Node(
-            package='linefit_ground_segmentation_ros',
-            executable='ground_segmentation_node',
-            output='screen',
-            parameters=[params_file])
-
+        package="linefit_ground_segmentation_ros",
+        executable="ground_segmentation_node",
+        additional_env=env,
+        output="screen",
+        parameters=[params_file],
+    )
 
     ld = LaunchDescription()
-
-    # Declare the launch options
     ld.add_action(node_start_cmd)
-
-
     return ld
