1918794526ad9e92f9deb74052e9070afe43d822
Kielas <c1470759@outlook.com>
2026-03-06 23:59:52 +0800

    添加了统计滤波和修改了裁切点云高度，增加了代价地图清理，修改了部分amcl参数和pointlio参数，（明天尝试导航，点云偏转后 纠正时任然容易存留）

---
+++ src/rm_navigation2/launch/amcl.launch.py
@@ -0,0 +1,32 @@
+import launch
+from launch import LaunchDescription
+from launch_ros.actions import Node
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+    nav2_yaml = os.path.join(get_package_share_directory('rm_navigation2'), 'config', 'amcl.yaml')
+
+
+    amcl_node = Node(
+        package='nav2_amcl',
+        executable='amcl',
+        name='amcl',
+        output='screen',
+        parameters=[nav2_yaml],
+    )
+
+    lifecycle_node = Node(
+        package='nav2_lifecycle_manager',
+        executable='lifecycle_manager',
+        name='lifecycle_manager_mapper',
+        output='screen',
+        parameters=[{'use_sim_time': False},
+                    {'autostart': True},
+                    {'node_names': ['amcl']}]
+    )
+
+    return LaunchDescription([
+        amcl_node,
+        lifecycle_node,
+    ])
--- src/rm_perception/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
+++ src/rm_perception/pointcloud_to_laserscan/launch/pointcloud_to_laserscan_launch.py
@@ -27,13 +27,13 @@ def generate_launch_description():
             parameters=[{
                 'target_frame': 'livox_frame',
                 'transform_tolerance': 0.01,
-                'min_height': -0.54,
-                'max_height': 0.01,
+                'min_height': -0.4,
+                'max_height': -0.2,
                 'angle_min': -3.14159,  # -M_PI/2
                 'angle_max': 3.14159,  # M_PI/2
                 'angle_increment': 0.0043,  # M_PI/360.0
                 'scan_time': 0.3333,
-                'range_min': 0.35,
+                'range_min': 0.4,
                 'range_max': 10.0,
                 'use_inf': True,
                 'inf_epsilon': 1.0
