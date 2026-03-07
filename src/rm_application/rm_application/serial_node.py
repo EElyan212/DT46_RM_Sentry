a11dc0bd77f88773d2e09c204f0096c2f0422ef6
Kielas <c1470759@outlook.com>
2026-03-08 01:28:29 +0800

    能用的nav2_params.yaml

--- src/rm_application/rm_application/serial_node.py
+++ src/rm_application/rm_application/serial_node.py
@@ -209,7 +209,7 @@ class SerialNode(Node):
                 serial_odo_msg.vy = vy
                 serial_odo_msg.yaw = yaw
                 self.pub_uart_receive_odo.publish(serial_odo_msg)
-                print(serial_odo_msg)
+                # print(serial_odo_msg)
                 #6.3 发布 Decision 消息
                 serial_decision_msg.header.stamp = self.get_clock().now().to_msg()
                 serial_decision_msg.color = detect_color
@@ -269,7 +269,7 @@ class SerialNode(Node):
             packet = data_payload + struct.pack("<H", checksum)
 
             self.serial.write(packet)
-            # print(header, linear_velocity_x,linear_velocity_y, gimbal_mode,yaw, pitch, can_fire)
+            print(header, linear_velocity_x,linear_velocity_y, gimbal_mode,yaw, pitch, can_fire)
         except Exception as e:
             self.get_logger().error(f"发送数据时出错: {str(e)}")
 
